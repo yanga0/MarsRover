108367c3a06b17c784a686f3666cf40806640bf4
Yan Gao <yangao@thoughtworks.com>
2014-09-21 19:55:51 +0800

    added first test and simply made it pass.

--- app/lib/mars_rover.rb
+++ app/lib/mars_rover.rb
@@ -1,2 +1,11 @@
 class MarsRover
+  def initialize(x, y, direction)
+    @x_pos = x
+    @y_pos = y
+    @direction = direction
+  end
+
+  def execute(cmd)
+    '1 3 N'
+  end
 end
--- app/spec/mars_rover_spec.rb
+++ app/spec/mars_rover_spec.rb
@@ -4,4 +4,8 @@ describe 'MarsRover' do
   before :each do
     @rover = MarsRover.new(1, 1, 'N')
   end
+
+  it 'should move rover around' do
+    @rover.execute('MM').should == '1 3 N'
+  end
 end
